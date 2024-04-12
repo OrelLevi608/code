9674e0c2bb9fb1a69c683deb1ab105b360fc9a1d
OrelLevi608 <157252229+OrelLevi608@users.noreply.github.com>
2024-04-12 14:54:53 +0300

    Delete שנה ב/node.js  -  לימוד בכיתה directory

--- שנה ב/node.js  -  לימוד בכיתה/token.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken'); // חיבור לספרייה
-
-// sign
-const token = jwt.sign({userid:5,email:"yogev466@gmail.com"},"orel123aaa",{expiresIn:'1h'}); // הצפנה 
-console.log(token);
-
-// verify
-const processVerify = jwt.verify(token, "orel123aaa"); // פיענוח
-console.log(processVerify);
